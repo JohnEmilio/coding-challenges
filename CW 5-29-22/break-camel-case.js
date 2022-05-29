d550103d621a7c162db9c932e243055325cb76a7
John Emilio <jl.emilio1@gmail.com>
2022-05-29 17:08:40 -0400

    CodeWars from 5-29-22 6kyu

---
+++ CW 5-29-22/break-camel-case.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5208f99aee097e6552000148/javascript
+/*Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
+function solution(string) {
+    return string.replace(/([a-z/])([A-Z])/g, `$1 $2`) 
+  
+  }
