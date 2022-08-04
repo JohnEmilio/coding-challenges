9db88a6cebb39a620ead087f0e1d2b9d4f554a89
John Emilio <jl.emilio1@gmail.com>
2022-08-04 13:24:05 -0400

    Challenges from 8-4-22

---
+++ 8-4-22/buying-a-car.js
@@ -0,0 +1,57 @@
+// https://www.codewars.com/kata/554a44516729e4d80b000012/javascript
+
+/*
+Let us begin with an example:
+
+A man has a rather old car being worth $2000. He saw a secondhand car being worth $8000. He wants to keep his old car until he can buy the secondhand one.
+
+He thinks he can save $1000 each month but the prices of his old car and of the new one decrease of 1.5 percent per month. Furthermore this percent of loss increases of 0.5 percent at the end of every two months. Our man finds it difficult to make all these calculations.
+
+Can you help him?
+
+How many months will it take him to save up enough money to buy the car he wants, and how much money will he have left over?
+
+Parameters and return of function:
+
+parameter (positive int or float, guaranteed) start_price_old (Old car price)
+parameter (positive int or float, guaranteed) start_price_new (New car price)
+parameter (positive int or float, guaranteed) saving_per_month 
+parameter (positive float or int, guaranteed) percent_loss_by_month
+
+nbMonths(2000, 8000, 1000, 1.5) should return [6, 766] or (6, 766)
+Detail of the above example:
+end month 1: percent_loss 1.5 available -4910.0
+end month 2: percent_loss 2.0 available -3791.7999...
+end month 3: percent_loss 2.0 available -2675.964
+end month 4: percent_loss 2.5 available -1534.06489...
+end month 5: percent_loss 2.5 available -395.71327...
+end month 6: percent_loss 3.0 available 766.158120825...
+return [6, 766] or (6, 766)
+where 6 is the number of months at the end of which he can buy the new car and 766 is the nearest integer to 766.158... (rounding 766.158 gives 766).
+
+Note:
+
+Selling, buying and saving are normally done at end of month. Calculations are processed at the end of each considered month but if, by chance from the start, the value of the old car is bigger than the value of the new one or equal there is no saving to be made, no need to wait so he can at the beginning of the month buy the new car:
+
+nbMonths(12000, 8000, 1000, 1.5) should return [0, 4000]
+nbMonths(8000, 8000, 1000, 1.5) should return [0, 0]
+We don't take care of a deposit of savings in a bank:-)
+*/
+
+function nbMonths(startPriceOld, startPriceNew, savings, percentLoss){
+    let runningTotal = startPriceNew - startPriceOld //By declaring new variables with the argument's, this makes a pure function as nothing that was passed as an arguement is altered in any way.
+    let month = 0
+    let totalSavings = 0
+    let monthlyPercent = percentLoss
+    
+    while(totalSavings < runningTotal){ //Since this loop will only run under these conditions, it will automatically weed out any edge cases such as, if startingOld = 12000 and startingNew = 4000, or the two prices were equal to each other.
+      month++
+      if(month % 2 == 0){
+        monthlyPercent += 0.5
+      }
+      runningTotal -= runningTotal*(monthlyPercent/100)
+      totalSavings += savings
+    }
+    
+    return [month, Math.round(totalSavings-runningTotal)] //At the end we will return how many months we would need to save and the rounded number of the totalSavings subtracted by the runningTotal (which is the depreciated amount of both cars.)
+  }
